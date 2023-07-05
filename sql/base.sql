9da694035f0946acf0ee058f0fed279f1b874250
tonymushah <tonyramanantsalama@gmail.com>
2023-07-05 05:17:33 +0300

    refactor: base.sql invocation order

--- sql/base.sql
+++ sql/base.sql
@@ -1,58 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";  
-
-create type ENUM_Sexe as ENUM('Homme', 'Femme');
-
-create table MOD_Personne(
-	id varchar(50) primary key default uuid_generate_v4(),
-	nom varchar(50) not null,
-	prenom varchar(50) not null,
-	numero_tel int not null,
-	cin varchar(50) not null,
-	Domicile varchar(50),
-	genre ENUM_Sexe not null,	
-	mot_de_passe varchar(256) not null,
-	email varchar(256)
-);
-
-create table MOD_Categorie(
-	id varchar(50) primary key default uuid_generate_v4(),
-	nom varchar(50) not null
-);
-
-create type ENUM_Status_Article as ENUM ('Dispo', 'Non_dispo');
-
-create table MOD_Article(
-	id varchar(50) primary key default uuid_generate_v4(),
-	nom varchar(50) not null,
-	personne_id varchar(50),
-	prix_unitaire int,
-	quantite int,
-	DateAjout timestamp,
-	description text,
-	status ENUM_Status_Article,
-	id varchar(50),
-	foreign key(id) references MOD_Categorie(id)
-);
-
-create table MOD_Article_Image(
-	id varchar(50) primary key default uuid_generate_v4(),
-	path_ varchar(256),
-	article_id varchar(50)
-);
-
-create type ENUM_Status_Ls_Dem as ENUM ('Confirme', 'Annulee');
-
-create table MOD_Liste_Demande(
-	id varchar(50) primary key default uuid_generate_v4(),
-	personne_id varchar(50),
-	article_id varchar(50),
-	status ENUM_Status_Ls_Dem
-);
-
---view maka produits par MOD_Categorie
-create database base;
-\c base;
-
 create type ENUM_Sexe as ENUM('Homme', 'Femme');
 
 CREATE TABLE personne(
@@ -109,6 +54,32 @@ create table listeDemande(
 	foreign key(idPersonne) references personne(idPersonne),
 	foreign key(idarticle) references article(idarticle)
 );
+
+--view detailarticle
+CREATE VIEW detailarticle AS 
+SELECT article.idarticle, titre, description, prix_unitaire, quantite, DateAjout, article.idproprietaire, p.nom, p.numero_tel
+FROM article
+JOIN personne AS p ON article.idproprietaire = p.idPersonne
+JOIN photoarticle AS pa ON article.idarticle = pa.idarticle;
+
+--getAllproduits
+select article.idarticle, titre, description, prix_unitaire, quantite, DateAjout, p.nom, p.numero_tel 
+from article 
+join photoarticle as pa on article.idarticle = pa.idarticle
+JOIN personne AS p ON article.idproprietaire = p.idPersonne;
+
+
+
+--getAllProduitsByCategorie
+select * from detailarticle join articlecategorie as ac on article.idarticle = ac.idarticle where idcategorie=1;
+
+--recherche par champ
+select * from detailarticle where titre=domino;
+
+--recherche multi categorie 
+select * from detailarticle join articlecategorie as ac on article.idarticle = ac.idarticle where idcategorie=1 and idcategorie=2;
+
+
 INSERT INTO personne (nom, prenom, numero_tel, cin, Domicile, genre, mot_de_passe, email)
 VALUES ('Doe', 'John', 123456789, '1234567890', '123 Street', 'Homme', 'motdepasse123', 'john.doe@example.com');
 
@@ -144,24 +115,3 @@ VALUES (2, 1, 'Confirme');
 
 INSERT INTO listeDemande (idPersonne, idarticle, status)
 VALUES (1, 2, 'Annulee');
-
-
---getAllproduits
-select article.idarticle, titre, description, prix_unitaire, quantite, DateAjout, p.nom, p.numero_tel from article 
-join photoarticle as pa on article.idarticle = pa.idarticle;
-
---getAllProduitsByCategorie
-select * from detailarticle join articlecategorie as ac on article.idarticle = ac.idarticle where idcategorie=1;
-
---view detailarticle
-CREATE VIEW detailarticle AS 
-SELECT article.idarticle, titre, description, prix_unitaire, quantite, DateAjout, article.idproprietaire, p.nom, p.numero_tel
-FROM article
-JOIN personne AS p ON article.idproprietaire = p.idPersonne
-JOIN photoarticle AS pa ON article.idarticle = pa.idarticle;
-
---recherche par champ
-select * from detailarticle where titre=domino;
-
---recherche multi categorie 
-select * from detailarticle join articlecategorie as ac on article.idarticle = ac.idarticle where idcategorie=1 and idcategorie=2;
---
+++ sql/user-db-setup.sql
@@ -0,0 +1,2 @@
+create user e_commerce_service with password 'etu001844';
+create database e_commerce_database owner e_commerce_service;
